a4b3ed25139263cc232dbc78cbf6c93e4995ebbb
penworth <120457748+penworth@users.noreply.github.com>
2023-06-14 15:13:24 +0100

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import UserOutput from "./component/userOutput";
 import Userinput from "./component/userInput";
 import "./App.css";
 
 function App() {
+  // adding usestate to upgrade the contents
+  const [userName, setuserName] = useState("wande coal");
+
+  const changeHandler = (e) => {
+    setuserName(e.target.value);
+  };
   return (
-    <div className="App">
-      <Userinput />
-      <UserOutput />
-      <UserOutput />
-    </div>
+    <>
+      <div>
+        <h1> I am moshood </h1>
+        <Userinput onChange={changeHandler} />
+
+        <UserOutput  userName={userName} />
+      </div>
+    </>
   );
 }
 
